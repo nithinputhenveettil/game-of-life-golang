fabe2e60e2afd63c4bb16a646c3a4f382cc06444
Nithin P <nithinp@qburst.com>
2020-03-09 23:15:55 +0530

    Added basic drawing functions

--- main.go
+++ main.go
@@ -37,6 +37,41 @@ func reset(gol *golGame) {
 	}
 }
 
+func litsenMouseClick(gol *golGame) {
+	if rl.IsMouseButtonPressed(0) {
+		points := rl.GetMousePosition()
+		if points.Y > float32(gol.size[1]-100) || points.Y < float32(50) {
+			return
+		}
+		i := -1
+		j := -1
+		x := 0
+		y := 50
+		for x < gol.size[0] {
+			if points.X >= float32(x) && points.X <= float32(x+25) {
+				j = x / 25
+				break
+			}
+			x += 25
+		}
+		for y < gol.size[1]-100 {
+			if points.Y >= float32(y) && points.Y <= float32(y+25) {
+				i = y/25 - 2
+				break
+			}
+			y += 25
+		}
+		if i == -1 || j == -1 {
+			return
+		}
+		if gol.game[i][j] {
+			gol.game[i][j] = false
+		} else {
+			gol.game[i][j] = true
+		}
+	}
+}
+
 func drawScreen(gol *golGame) {
 	rl.ClearBackground(rl.White)
 
@@ -55,6 +90,18 @@ func drawScreen(gol *golGame) {
 	rl.DrawText("Generations : "+strconv.Itoa(gol.generations), int32(50), int32(gol.size[1]-40), 20, rl.Black)
 	rl.DrawText("Alive : "+strconv.Itoa(gol.alive), int32(gol.size[0]/2), int32(gol.size[1]-40), 20, rl.Black)
 	rl.DrawText("Dead : "+strconv.Itoa(gol.dead), int32(gol.size[0]-140), int32(gol.size[1]-40), 20, rl.Black)
+
+	for i, a := range gol.game {
+		for j := range a {
+			if gol.game[i][j] {
+				var x, y int
+				x = j*25 + 1
+				y = i*25 + 50 + 1
+				rl.DrawRectangle(int32(x), int32(y), 22, 22, rl.Black)
+			}
+		}
+	}
+
 }
 
 func main() {
@@ -64,6 +111,7 @@ func main() {
 	rl.InitWindow(int32(gol.size[0]), int32(gol.size[1]), gol.name)
 	rl.SetTargetFPS(60)
 	for !rl.WindowShouldClose() {
+		litsenMouseClick(gol)
 		rl.BeginDrawing()
 		drawScreen(gol)
 		rl.EndDrawing()
